ec4e55d180826c5fd3f070420c78f2553c9ebd43
qndls42 <qndls42@naver.com>
2024-01-02 22:45:40 +0900

    feat: Add Webtoon card list

--- toonflix/lib/main.dart
+++ toonflix/lib/main.dart
@@ -1,17 +1,28 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:toonflix/screens/home_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ToonflixApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class ToonflixApp extends StatelessWidget {
+  const ToonflixApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Container(),
+      scrollBehavior: const MaterialScrollBehavior().copyWith(
+        dragDevices: {
+          PointerDeviceKind.mouse,
+          PointerDeviceKind.touch,
+          PointerDeviceKind.stylus,
+          PointerDeviceKind.unknown,
+        },
+      ),
+      home: HomeScreen(),
     );
   }
 }
